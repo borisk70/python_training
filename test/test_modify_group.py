c8b27b776f9a085395a97430e561116a440b0d45
Борис Князев <bsl0007@yandex.ru>
2022-08-31 11:17:13 +0300

    add checking group exists before delete and modify group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,10 +3,14 @@ from model.group import Group
 
 def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
+    if app.group.count() == 0:
+       app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="New group"))
     app.session.logout()
 
 def test_modify_group_header(app):
     app.session.login(username="admin", password="secret")
+    if app.group.count() == 0:
+       app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(header="New header"))
     app.session.logout()
