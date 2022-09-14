17299433386351d66acbcb4bdc9458fb0234c17a
Борис Князев <bsl0007@yandex.ru>
2022-09-14 18:23:16 +0300

    add options command line for browser type and base url

--- fixture/application.py
+++ fixture/application.py
@@ -4,17 +4,24 @@ from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+           self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecongnized browser %s" % browser)
         #self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
-
+        self.base_url = base_url
 
     def open_home_page(self):
         # open home page
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
