f760f4d576d645ff99444b0952fc6881d03be975
Борис Князев <bsl0007@yandex.ru>
2022-09-21 19:48:15 +0300

    realize generator of test data

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,9 +2,10 @@
 
 from model.group import Group
 import pytest
-#from sys import maxsize
-import random
-import string
+from data.add_group import constant as testdata
+#from data.add_group import testdata
+#import random
+#import string
 
 
 
@@ -14,28 +15,17 @@ import string
   #    fixture = Application()
    #   request.addfinalizer(fixture.destroy)
    #   return fixture
-def random_string(prefix, maxlen):
-        #symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-        symbols = string.ascii_letters + string.digits + " "*10
-        return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+#def random_string(prefix, maxlen):
+       #symbols = string.ascii_letters + string.digits + " "*10
+        #return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-#testdata = [
- #       Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20)),
- #       Group(name="", header="", footer="")
- #   ]
+#testdata = [Group(name="", header="", footer="")] + [
+ #       Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+  #      for i in range(5)
+#]
 
-testdata = [Group(name="", header="", footer="")] + [
-        Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-        for i in range(5)
-]
-
-#testdata = [
-#           Group(name=name, header=header, footer=footer)
-#           for name in ["", random_string("name", 10)]
-#           for header in ["", random_string("header", 20)]
-#           for footer in ["", random_string("footer", 20)]
-#    ]
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
         app.session.login(username="admin", password="secret")
