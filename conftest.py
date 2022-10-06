c50531e87e4b66bb73e3e7b6b44bb4855d79de87
Борис Князев <bsl0007@yandex.ru>
2022-10-06 19:45:28 +0300

    loading objects from databse and creating switchable checking user interface

--- conftest.py
+++ conftest.py
@@ -6,28 +6,46 @@ import json
 import os.path
 import importlib
 import jsonpickle
+from fixture.db import DbFixture
 
 
 
 fixture = None
 target = None
 
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
+
+
 #@pytest.fixture(scope="session")
 @pytest.fixture()
 def app(request):
      global fixture
-     global target
+     #global target
      browser = request.config.getoption("--browser")
-     if target is None:
-         config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
-         with open(config_file) as f:
-              target = json.load(f)
-     #base_url = request.config.getoption("--baseUrl")
+     web_config = load_config(request.config.getoption("--target"))['web']
      if fixture is None or not fixture.is_valid():
-         fixture = Application(browser=browser, base_url=target['baseUrl'])
-     #fixture.session.ensure_login(username="login", password="secret")
-     fixture.session.ensure_login(username=target['username'], password=target['password'])
+         fixture = Application(browser=browser, base_url=web_config['baseUrl'])
+     fixture.session.ensure_login(username=web_config['username'], password=web_config['password'])
      return fixture
+
+#@pytest.fixture(scope="session")
+@pytest.fixture()
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))['db']
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
+
+
 
 @pytest.fixture(scope="session", autouse=True)
 #@pytest.fixture()
@@ -37,11 +55,15 @@ def stop(request):
         fixture.destroy()
      request.addfinalizer(fin)
      return fixture
+@pytest.fixture
+def check_ui(request)
+    return request.config.getoption("--check_ui")
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     #parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
     parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
 
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
